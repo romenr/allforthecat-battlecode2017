e481242be75cd77fef09ac0e4e2fa7ac6efc43dc
Romen Rene <romen.rene@gmail.com>
2017-01-21 19:21:16 +0100

    Added Variables for every Robot

--- src/thecat/RobotPlayer.java
+++ src/thecat/RobotPlayer.java
@@ -1,10 +1,15 @@
 package thecat;
 
 import battlecode.common.*;
+import gamemechanics.Debug;
 import robots.*;
 
 public strictfp class RobotPlayer {
 	public static RobotController rc;
+	public static MapLocation[][] initialArchonLocations;
+	public static Team ally;
+	public static Team opponent;
+	public static int numberOfArchons;
 
 	/**
 	 * run() is the method that is called when a robot is instantiated in the
@@ -16,6 +21,20 @@ public strictfp class RobotPlayer {
 		// from this robot,
 		// and to get information on its current status.
 		RobotPlayer.rc = rc;
+		
+		// Save the Team of the Robot and the Opponents
+		ally = rc.getTeam();
+		opponent = ally.opponent();
+		
+		// Get the Initial Archon Locations
+		initialArchonLocations = new MapLocation[2][];
+		initialArchonLocations[ally.ordinal()] = rc.getInitialArchonLocations(ally);
+		initialArchonLocations[opponent.ordinal()] = rc.getInitialArchonLocations(opponent);
+		
+		// Save the Number of Archons per Team
+		numberOfArchons = initialArchonLocations[0].length;
+		
+		Debug.debug_println("Startup Bytecosts: " + Clock.getBytecodeNum());
 
 		// Here, we've separated the controls into a different method for each
 		// RobotType.
